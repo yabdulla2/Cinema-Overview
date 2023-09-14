5300d8653c380504140fd140c30b5d919df482d1
yabdulla2 <118770106+yabdulla2@users.noreply.github.com>
2023-09-13 20:37:56 -0500

    Update script.js
    Removed comments

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const KEY = "3fd2be6f0c70a2a598f084ddfb75487c";
-// For educational purposes only - DO NOT USE in production
-// Request your own key for free: https://developers.themoviedb.org/3
 const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${KEY}&page=1`;
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${KEY}&query=`;
